6232e6eb460b798b39306e5511dca69785912afe
Deanna Hood <deanna.m.hood@gmail.com>
2014-04-17 15:56:21 +0200

    Put shape learner into a class

--- shape_learning/scripts/shape_learning.py
+++ shape_learning/scripts/shape_learning.py
@@ -30,12 +30,14 @@ doGroupwiseComparison = True; #instead of pairwise comparison with most recent t
 
 #trajectory publishing parameters
 FRAME = 'writing_surface';  #Frame ID to publish points in
+FEEDBACK_TOPIC = 'shape_feedback'; #Name of topic to receive feedback on
+SHAPE_TOPIC = 'write_traj'; #Name of topic to publish shapes to
 t0 = 0.5;                   #Time allowed for the first point in traj (seconds)
 dt = 0.1;                   #Seconds between points in traj
 delayBeforeExecuting = 0.5; #How far in future to request the traj be executed (to account for transmission delays and preparedness)
 sizeScale = 0.06            #Desired max dimension of shape (metres)
 
-pub_traj = rospy.Publisher('write_traj', Path);
+pub_traj = rospy.Publisher(SHAPE_TOPIC, Path);
 
 rospy.init_node("shape_learner");
 
@@ -59,115 +61,152 @@ def make_traj_msg(shape):
     
 ###
 
-bestParamValue = [];
-converged = False;
-numIters = 0;
-### ----------------------------------------------------------- CALLBACK
-def on_feedback(feedback):
-    global bestParamValue #@todo: avoid global variables
-    global numIters
-    global converged
-    global newParamValue
-    global bounds
-    
-    if(doGroupwiseComparison):
-        global params_sorted
-        global shapeToParamMapping
-    
-    #----------------------------------------------- respond to feedback
-    if(numIters == 0): #interpret feedback as whether or not the first shape is good enough
-        if(feedback.data == 'shape0'):
-            converged = True;
-    else:
-        bestShape = feedback.data;
+import bisect
+class ShapeLearner:
+    def __init__(self):
+        self.converged = False;
+        self.numIters = 0;
+        self.newParamValue = []; #this is just for the pair-wise one
         
+### ----------------------------------------------------- START LEARNING        
+    def startLearning(self, startingBounds):
+        self.bounds = startingBounds;
+        
+        #make initial shape
+        [shape, self.bestParamValue] = shapeModeler.makeRandomShapeFromUniform(paramToVary, self.bounds);
+        shape = ShapeModeler.normaliseShape(shape);
+        print('Bounds: '+str(self.bounds));
+        print('Test param: '+str(self.bestParamValue));
         if(doGroupwiseComparison):
-            bestShape = int(bestShape[5:]) #message is 'shape0' or 'shape1' etc
-            bestParamValue = shapeToParamMapping[bestShape];
-            bestParamValue_index = bisect.bisect(params_sorted,bestParamValue) - 1; #indexing seems to start at 1 with bisect
-            bounds = [params_sorted[bestParamValue_index-1],params_sorted[bestParamValue_index+1] ];
-            #restrict bounds if they were caused by other shapes, because it must be sufficiently different to said shape(s)
-            if((bestParamValue_index-1) > 0): #not the default min
-                bounds[0] += diffThresh;
-            if((bestParamValue_index+1) < (len(params_sorted)-1)): #not the default max
-                bounds[1] -= diffThresh;
-
-        else: #do pairwise comparison with most recent shape and previous
-            #restrict limits
-            if( bestShape == 'new' ):   #new shape is better
-                worstParamValue = bestParamValue;
-                bestParamValue = newParamValue;  
-            else:                   #new shape is worse
-                worstParamValue = newParamValue;
-                bestParamValue = bestParamValue;
-                
-            if( worstParamValue == min(bestParamValue,newParamValue) ): #shape with lower value is worse
-                bounds[0] = worstParamValue; #increase min bound to worst so we don't try any lower
-            else: #shape with higher value is worse
-                bounds[1] = worstParamValue; #decrease max bound to worst so we don't try any higher
+            self.params_sorted = [self.bounds[0], self.bounds[1]];
+            bisect.insort(self.params_sorted, self.bestParamValue);
+            self.shapeToParamMapping = [self.bestParamValue];
         
-        #continue if there are more shapes to try which are different enough
-        if((abs(bounds[1]-bestParamValue)-diffThresh < tol) and (abs(bestParamValue-bounds[0])-diffThresh) < tol):
-            converged = True;
-    
-    #------------------------------------------------ continue iterating
-    numIters+=1;
-           
-    #try again if shape is not good enough
-    if(not converged):
+        #publish shape and get feedback
+        if(simulatedFeedback):
+            #pretend that first one wasn't good enough
+            feedback = String();
+            feedback.data = "no";
+            self.on_feedback(feedback); #call callback manually
+            
+        else:
+            traj = make_traj_msg(shape);
+            pub_traj.publish(traj);
+            feedback_subscriber = rospy.Subscriber(FEEDBACK_TOPIC, String, self.on_feedback);
+            rospy.spin();
         
+### ----------------------------------------------- MAKE DIFFERENT SHAPE        
+    def makeShapeDifferentTo(self, paramValue):
         #make new shape to compare with
-        [newShape, newParamValue] = shapeModeler.makeRandomShapeFromTriangular(paramToVary, bounds, bestParamValue);
+        [newShape, newParamValue] = shapeModeler.makeRandomShapeFromTriangular(paramToVary, self.bounds, self.bestParamValue);
         #ensure it is significantly different
         numAttempts = 1;
-        while(abs(newParamValue - bestParamValue) < diffThresh and numAttempts < maxNumAttempts):
-            [newShape, newParamValue] = shapeModeler.makeRandomShapeFromTriangular(paramToVary, bounds, bestParamValue);
+        while(abs(newParamValue - paramValue) < diffThresh and numAttempts < maxNumAttempts):
+            [newShape, newParamValue] = shapeModeler.makeRandomShapeFromTriangular(paramToVary, self.bounds, self.bestParamValue);
             numAttempts+=1;      
         
         if(numAttempts>=maxNumAttempts): #couldn't find a 'different' shape in range
             print('Oh no!'); #this should be prevented by the convergence test below
-         
+        
+        #store it as an attempt
         if(doGroupwiseComparison):
-            bisect.insort(params_sorted, newParamValue);
-            shapeToParamMapping.append(newParamValue);
+            bisect.insort(self.params_sorted, newParamValue);
+            self.shapeToParamMapping.append(newParamValue);
+            
+        return newShape, newParamValue   
         
-        #publish shape and get feedback
-        print('Bounds: '+str(bounds));
-        print('Test param: '+str(newParamValue));        
-        
+### ------------------------------------------------------ PUBLISH SHAPE        
+    def publishShapeAndWaitForFeedback(self, shape, newParamValue):
         if(simulatedFeedback):
             if(args.show):
                 plt.figure(1);
-                ShapeModeler.normaliseAndShowShape(newShape);
+                ShapeModeler.normaliseAndShowShape(shape);
                 time.sleep(1.3);
             
             #temporary code in place of feedback from user: go towards goal parameter value
             goalParamValue =  numpy.float64(-0.22);#-1.5*parameterVariances[paramToVary-1];
             if(doGroupwiseComparison):
-                #print(params_sorted)
                 errors = numpy.ndarray.tolist(abs(shapeToParamMapping-goalParamValue));
                 bestShape_idx = errors.index(min(errors));
-                #print(bestShape_idx)
                 feedback.data = 'shape'+str(bestShape_idx);
             else:   
-                errors = [abs(bestParamValue- goalParamValue), abs(newParamValue- goalParamValue)];
+                errors = [abs(self.bestParamValue- goalParamValue), abs(newParamValue- goalParamValue)];
                 names = ('old', 'new');
                 bestShape_idx = errors.index(min(errors));
                 feedback.data = names[bestShape_idx];
-            on_feedback(feedback);
+            self.on_feedback(feedback);
         else:
-            traj = make_traj_msg(newShape);
+            traj = make_traj_msg(shape);
             pub_traj.publish(traj);
             
-    else:          
-        print('Converged');  
-        if(args.show):      
-            plt.show(block=True);
-     
+### ------------------------------------------------ RESPOND TO FEEDBACK                
+    def respondToFeedback(self, bestShape):
+        if(doGroupwiseComparison):
+            bestShape = int(bestShape[5:]) #message is 'shape0' or 'shape1' etc
+            self.bestParamValue = self.shapeToParamMapping[bestShape];
+            bestParamValue_index = bisect.bisect(self.params_sorted,self.bestParamValue) - 1; #indexing seems to start at 1 with bisect
+            self.bounds = [self.params_sorted[bestParamValue_index-1],self.params_sorted[bestParamValue_index+1] ];
+            #restrict bounds if they were caused by other shapes, because it must be sufficiently different to said shape(s)
+            if((bestParamValue_index-1) > 0): #not the default min
+                self.bounds[0] += diffThresh;
+            if((bestParamValue_index+1) < (len(self.params_sorted)-1)): #not the default max
+                self.bounds[1] -= diffThresh;
+
+        else: #do pairwise comparison with most recent shape and previous
+            #restrict limits
+            if( bestShape == 'new' ):   #new shape is better
+                worstParamValue = self.bestParamValue;
+                bestParamValue = self.newParamValue;  
+            else:                   #new shape is worse
+                worstParamValue = self.newParamValue;
+                bestParamValue = self.bestParamValue;
+                
+            if( worstParamValue == min(self.bestParamValue,self.newParamValue) ): #shape with lower value is worse
+                self.bounds[0] = worstParamValue; #increase min bound to worst so we don't try any lower
+            else: #shape with higher value is worse
+                self.bounds[1] = worstParamValue; #decrease max bound to worst so we don't try any higher
+                
+        #return self.bounds
+        
+### ----------------------------------------------------------- CALLBACK       
+    def on_feedback(self, feedback):        
+        
+        #------------------------------------------- respond to feedback
+        if(self.numIters == 0): #interpret feedback as whether or not the first shape is good enough
+            if(feedback.data == 'shape0'):
+                self.converged = True;
+        else:
+            bestShape = feedback.data;
+            
+            self.respondToFeedback(bestShape); #update bounds and bestParamValue
+            
+            #continue if there are more shapes to try which are different enough
+            if((abs(self.bounds[1]-self.bestParamValue)-diffThresh < tol) and (abs(self.bestParamValue-self.bounds[0])-diffThresh) < tol):
+                self.converged = True;
+        
+        #-------------------------------------------- continue iterating
+        self.numIters+=1;
+               
+        #try again if shape is not good enough
+        if(not self.converged):
+            [newShape, newParamValue] = self.makeShapeDifferentTo(self.bestParamValue);
+            self.newParamValue = newParamValue;
+            print('Bounds: '+str(self.bounds));
+            print('Test param: '+str(newParamValue));        
+            
+            #publish shape and get feedback
+            self.publishShapeAndWaitForFeedback(newShape,newParamValue);
+                
+        else:          
+            print('Converged');  
+            if(args.show):      
+                plt.show(block=True);
+         
 ### --------------------------------------------------------------- MAIN
         
 if __name__ == "__main__":
 
+    #parse parameters
     import argparse
     parser = argparse.ArgumentParser(description='Publish shapes on the \
     /shapes_to_draw topic and adapt them based on feedback received on the /shape_feedback topic');
@@ -177,37 +216,18 @@ if __name__ == "__main__":
 
     args = parser.parse_args();
     
+    #analyse database of shapes
     shapeModeler = ShapeModeler();
     shapeModeler.makeDataMatrix(args.file);
     shapeModeler.performPCA(numPrincipleComponents);
 
+    #learn parameter of shape
     parameterVariances = shapeModeler.getParameterVariances();
     bounds = numpy.array(stdDevMultiples)*parameterVariances[paramToVary-1];
     
     if(args.show):
         plt.ion(); #to plot one shape at a time
 
-    #make initial shape
-    [shape, bestParamValue] = shapeModeler.makeRandomShapeFromUniform(paramToVary, bounds);
-    shape = ShapeModeler.normaliseShape(shape);
-    print('Bounds: '+str(bounds));
-    print('Test param: '+str(bestParamValue));
-    if(doGroupwiseComparison):
-        import bisect
-        params_sorted = [bounds[0], bounds[1]];
-        bisect.insort(params_sorted, bestParamValue);
-        shapeToParamMapping = [bestParamValue];
-    
-    #publish shape and get feedback
-    if(simulatedFeedback):
-        #pretend that first one wasn't good enough
-        feedback = String();
-        feedback.data = "no";
-        on_feedback(feedback); #call callback manually
-        
-    else:
-        traj = make_traj_msg(shape);
-        pub_traj.publish(traj);
-        feedback_subscriber = rospy.Subscriber('shape_feedback', String, on_feedback);
-        rospy.spin();
+    shapeLearner = ShapeLearner();
+    shapeLearner.startLearning(bounds);
         
