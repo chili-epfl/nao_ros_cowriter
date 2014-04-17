1c2d28b26ecc4d8228c609d29e6a5dee2af62b21
Deanna Hood <deanna.m.hood@gmail.com>
2014-04-17 13:55:22 +0200

    Allow for the best of *all* shapes to be selected, not just of previous two

--- shape_learning/scripts/shape_learning.py
+++ shape_learning/scripts/shape_learning.py
@@ -21,18 +21,19 @@ from std_msgs.msg import String
 #shape learning parameters
 paramToVary = 4;            #Natural number between 1 and numPrincipleComponents, representing which principle component to vary from the template
 numPrincipleComponents = 5; #Number of principle components to keep during PCA of dataset
-maxNumAttempts = 500;       #Allowed attempts to draw a new shape which is significantly different to the previous one but still within the range
-diffThresh = 0.25;          #How different two shapes' parameters need to be to be published for comparison
+maxNumAttempts = 1000;      #Allowed attempts to draw a new shape which is significantly different to the previous one but still within the range (just a precaution; sampling should be theoretically possible)
+diffThresh = 0.2;           #How different two shapes' parameters need to be to be published for comparison
 tol = 1e-2;                 #Tolerance on convergence test
-stdDevMultiples = [-2, 4];  #Starting bounds for paramToVary, as multiples of the parameter's observed standard deviation in the dataset
+stdDevMultiples = [-6, 6];  #Starting bounds for paramToVary, as multiples of the parameter's observed standard deviation in the dataset
 simulatedFeedback = False;  #Simulate user feedback as whichever shape is closest to goal parameter value
-    
+doGroupwiseComparison = True; #instead of pairwise comparison with most recent two shapes
+
 #trajectory publishing parameters
-FRAME = 'writing_surface';
-dt = 0.1;                   #seconds between points in traj
-t0 = 0.5;                   #time for the robot to get to the first point in traj (seconds)
-delayBeforeExecuting = 0.5; #how far in future to request the traj be executed (to account for transmission delays and preparedness)
-sizeScale = 0.1;            #desired max dimension of shape (metres)
+FRAME = 'writing_surface';  #Frame ID to publish points in
+t0 = 0.5;                   #Time allowed for the first point in traj (seconds)
+dt = 0.1;                   #Seconds between points in traj
+delayBeforeExecuting = 0.5; #How far in future to request the traj be executed (to account for transmission delays and preparedness)
+sizeScale = 0.06            #Desired max dimension of shape (metres)
 
 pub_traj = rospy.Publisher('write_traj', Path);
 
@@ -67,24 +68,43 @@ def on_feedback(feedback):
     global numIters
     global converged
     global newParamValue
+    global bounds
+    
+    if(doGroupwiseComparison):
+        global params_sorted
+        global shapeToParamMapping
+    
     #----------------------------------------------- respond to feedback
     if(numIters == 0): #interpret feedback as whether or not the first shape is good enough
-        if(feedback.data == 'yes'):
+        if(feedback.data == 'shape0'):
             converged = True;
     else:
         bestShape = feedback.data;
-        #restrict limits
-        if( bestShape == 'new' ):   #new shape is better
-            worstParamValue = bestParamValue;
-            bestParamValue = newParamValue;  
-        else:                   #new shape is worse
-            worstParamValue = newParamValue;
-            bestParamValue = bestParamValue;
-            
-        if( worstParamValue == min(bestParamValue,newParamValue) ): #shape with lower value is worse
-            bounds[0] = worstParamValue; #increase min bound to worst so we don't try any lower
-        else: #shape with higher value is worse
-            bounds[1] = worstParamValue; #decrease max bound to worst so we don't try any higher
+        
+        if(doGroupwiseComparison):
+            bestShape = int(bestShape[5:]) #message is 'shape0' or 'shape1' etc
+            bestParamValue = shapeToParamMapping[bestShape];
+            bestParamValue_index = bisect.bisect(params_sorted,bestParamValue) - 1; #indexing seems to start at 1 with bisect
+            bounds = [params_sorted[bestParamValue_index-1],params_sorted[bestParamValue_index+1] ];
+            #restrict bounds if they were caused by other shapes, because it must be sufficiently different to said shape(s)
+            if((bestParamValue_index-1) > 0): #not the default min
+                bounds[0] += diffThresh;
+            if((bestParamValue_index+1) < (len(params_sorted)-1)): #not the default max
+                bounds[1] -= diffThresh;
+
+        else: #do pairwise comparison with most recent shape and previous
+            #restrict limits
+            if( bestShape == 'new' ):   #new shape is better
+                worstParamValue = bestParamValue;
+                bestParamValue = newParamValue;  
+            else:                   #new shape is worse
+                worstParamValue = newParamValue;
+                bestParamValue = bestParamValue;
+                
+            if( worstParamValue == min(bestParamValue,newParamValue) ): #shape with lower value is worse
+                bounds[0] = worstParamValue; #increase min bound to worst so we don't try any lower
+            else: #shape with higher value is worse
+                bounds[1] = worstParamValue; #decrease max bound to worst so we don't try any higher
         
         #continue if there are more shapes to try which are different enough
         if((abs(bounds[1]-bestParamValue)-diffThresh < tol) and (abs(bestParamValue-bounds[0])-diffThresh) < tol):
@@ -107,10 +127,13 @@ def on_feedback(feedback):
         if(numAttempts>=maxNumAttempts): #couldn't find a 'different' shape in range
             print('Oh no!'); #this should be prevented by the convergence test below
          
-        #publish shape and get feedback
+        if(doGroupwiseComparison):
+            bisect.insort(params_sorted, newParamValue);
+            shapeToParamMapping.append(newParamValue);
         
-        print(newParamValue);
-        print(bounds);        
+        #publish shape and get feedback
+        print('Bounds: '+str(bounds));
+        print('Test param: '+str(newParamValue));        
         
         if(simulatedFeedback):
             if(args.show):
@@ -119,11 +142,18 @@ def on_feedback(feedback):
                 time.sleep(1.3);
             
             #temporary code in place of feedback from user: go towards goal parameter value
-            goalParamValue =  -1.5*parameterVariances[paramToVary-1];
-            errors = [abs(bestParamValue- goalParamValue), abs(newParamValue- goalParamValue)];
-            names = ('old', 'new');
-            bestShape_idx = errors.index(min(errors));
-            feedback.data = names[bestShape_idx];
+            goalParamValue =  numpy.float64(-0.22);#-1.5*parameterVariances[paramToVary-1];
+            if(doGroupwiseComparison):
+                #print(params_sorted)
+                errors = numpy.ndarray.tolist(abs(shapeToParamMapping-goalParamValue));
+                bestShape_idx = errors.index(min(errors));
+                #print(bestShape_idx)
+                feedback.data = 'shape'+str(bestShape_idx);
+            else:   
+                errors = [abs(bestParamValue- goalParamValue), abs(newParamValue- goalParamValue)];
+                names = ('old', 'new');
+                bestShape_idx = errors.index(min(errors));
+                feedback.data = names[bestShape_idx];
             on_feedback(feedback);
         else:
             traj = make_traj_msg(newShape);
@@ -160,7 +190,13 @@ if __name__ == "__main__":
     #make initial shape
     [shape, bestParamValue] = shapeModeler.makeRandomShapeFromUniform(paramToVary, bounds);
     shape = ShapeModeler.normaliseShape(shape);
-    print(bestParamValue);
+    print('Bounds: '+str(bounds));
+    print('Test param: '+str(bestParamValue));
+    if(doGroupwiseComparison):
+        import bisect
+        params_sorted = [bounds[0], bounds[1]];
+        bisect.insort(params_sorted, bestParamValue);
+        shapeToParamMapping = [bestParamValue];
     
     #publish shape and get feedback
     if(simulatedFeedback):
